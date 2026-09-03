321280020e9c2d6c5c8593507f08b22c3ef5406f
fatimazr24 <zr.fatima11@gmail.com>
2026-09-03 23:13:45 +0530

    3

--- master_ard/master.ino
+++ master_ard/master.ino
@@ -10,7 +10,7 @@ const int servo_pin=9;
 const int buzz_pin=12;
 
 //servo obj
-Servo s_obj;
+Servo servo_obj;
 
 //data holding vars
 int temp=0, gas=0, ldr=0;
@@ -19,10 +19,32 @@ int cur_state=0;
 
 void setup()
 {
-  
+  Serial.begin(9600);
+  Wire.begin();
+
+  //pin initalising
+  pinMode(buzz_pin, OUTPUT);
+  servo_obj.attach(servo_pin);
 }
 
 void loop()
 {
   
+
+
+}
+
+void s_data()
+{
+  //reading sensor data
+    temp=analogRead(temp_pin);
+    ldr=analogRead(ldr_pin);
+    gas=analogRead(gas_pin);
+
+  //getting temp value from dig. value
+    double temp_c=(temp*5.0)/1023; 
+    temp_c-=0.5;
+    temp_c*=100.0
+
+    
 }
--- slave_ard/slave.ino
+++ slave_ard/slave.ino
@@ -5,3 +5,16 @@
 LiquidCrystal_I2C lcd(0x20,16,2);
 
 const int ir_pin=7;
+
+void setup()
+{
+    
+    Serial.begin(9600);
+    Wire.begin(13); #slavery abolished w the 13th amendment 
+
+}
+ 
+void loop()
+{
+
+}
