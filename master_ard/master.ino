d23d759275703346cdcbc5e17fb39fb64f95c3ad
fatimazr24 <zr.fatima11@gmail.com>
2026-09-03 18:56:19 +0530

    1

---
+++ master_ard/master.ino
@@ -0,0 +1,25 @@
+
+#include <Wire.h>
+#include <Servo.h>
+
+//glob vars
+const ldr_pin= A0;
+const gas_pin=A1;
+const temp_pin=A2;
+const servo_pin=9;
+const buzz_pin=12;
+
+//data holding vars
+int temp=0, gas=0, ldr=0;
+
+int cur_state=0;
+
+void setup()
+{
+  
+}
+
+void loop()
+{
+  
+}
